d85eb67f1946b8121b19e48c284bb999b261427a
Vladimir Morozov <mrvlmor@gmail.com>
2019-08-12 22:02:09 +0300

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/mywidget/MyWidget.java
@@ -0,0 +1,29 @@
+package ru.geekbrains.mywidget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+public class MyWidget extends AppWidgetProvider {
+
+    private final static String ExtraMsg = "msg";
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.my_widget);
+        // Здесь обновим текст, будем показывать номер виджета
+        views.setTextViewText(R.id.appwidget_text, String.format("%s - %d", widgetText, appWidgetId));
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+}
+
